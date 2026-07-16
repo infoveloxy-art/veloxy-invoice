670c8f469241b0b3abd0da629f4d8d9c61474f1c
infoveloxy-art <infoveloxy@gmail.com>
2026-07-16 21:07:22 +0200

    Update customer.py

--- backend/app/models/customer.py
+++ backend/app/models/customer.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -6,9 +7,9 @@ from app.database import Base
 class Customer(Base):
     __tablename__ = "customers"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
 
-    company = Column(String(255))
+    company = Column(String(255), nullable=False)
 
     address = Column(String(255))
 
@@ -17,3 +18,8 @@ class Customer(Base):
     dic = Column(String(30))
 
     email = Column(String(255))
+
+    invoices = relationship(
+        "Invoice",
+        back_populates="customer"
+    )
