015d5c1b7cc5e49763ee593cf68035db7c0f1511
infoveloxy-art <infoveloxy@gmail.com>
2026-07-16 20:42:18 +0200

    Create settings.py

---
+++ backend/app/settings.py
@@ -0,0 +1,10 @@
+COMPANY = {
+    "name": "Veloxy Trade s.r.o.",
+    "address": "Uralská 689/7, 160 00 Praha 6",
+    "ico": "19899645",
+    "dic": "CZ19899645",
+    "email": "info@veloxy.cz",
+    "bank": "Česká spořitelna",
+    "iban": "CZ0408000000006610568389",
+    "swift": "GIBACZPX"
+}
