4beb9b7bb5ec580f8af63385248502375ad8c2a4
infoveloxy-art <infoveloxy@gmail.com>
2026-07-16 20:55:58 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,29 +1,27 @@
 from fastapi import FastAPI
 
 from app.api import router
-
 from app.config import settings
+from app.init_db import create_database
 
 app = FastAPI(
-
     title=settings.APP_NAME,
-
-    version=settings.VERSION
-
+    version=settings.VERSION,
 )
+
+
+@app.on_event("startup")
+def startup():
+    create_database()
+
 
 app.include_router(router)
 
 
 @app.get("/")
 def root():
-
     return {
-
         "application": settings.APP_NAME,
-
         "version": settings.VERSION,
-
         "status": "Running"
-
     }
